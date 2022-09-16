0df8c2a8e996b9547b34f4402976800616651c24
Antonnfs <anton.nfs1@gmail.com>
2022-09-16 10:04:23 +0300

    ExtraReducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useAppDispach, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreators';
+
+import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
+  const dispatch = useAppDispach()
+  const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
+  
   return (
-    <div className="App">
-      work
+    <div className=" text-amber-700 mx-auto container flex content-center justify-center flex-col">
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {/* {!error && !isLoading && JSON.stringify(users, null, 2)} */}
+      {users.map(user => (
+        <div key={user.id} className=' text-center'>
+          <h1>Name: {user.name}</h1>
+          <p>id: {user.id}</p>
+          <p>email: {user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
