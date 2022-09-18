86e09231e7b5799f0a8e3e84538214ea2f4c2a25
Antonnfs <anton.nfs1@gmail.com>
2022-09-18 22:48:29 +0300

    JSON Server

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -7,7 +7,7 @@ interface PostItemProps {
 
 export const PostItem: FC<PostItemProps> = ({post}) => {
 	return (
-		<div className=' flex gap-5 m-5 p-2 border'>
+		<div className=' flex justify-between gap-5 m-5 p-2 border'>
 			<div>
 				<h1 className='my-2 text-black'>{post.id}. {post.title}</h1>
 				<p>{post.body}</p>
