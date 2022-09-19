b41febe2d76fdc51623274ea09cdb54d63b11b34
Antonnfs <anton.nfs1@gmail.com>
2022-09-19 10:52:51 +0300

    JSON Server

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -5,7 +5,9 @@ import { PostItem } from './PostItem'
 
 export default function PostContainer() {
 	const [limit, setLimit] = useState(100)
-	const [createPost, {error: createError, isLoading: isCreateLoadimg}] = postAPI.useCreatePostMutation()
+	const [createPost, {error: createError, isLoading: isCreateLoading}] = postAPI.useCreatePostMutation()
+	const [updatePost, {error: updateError, isLoading: isUpdateLoading}] = postAPI.useUpdatePostMutation()
+	const [deletePost, {error: deleteError, isLoading: isDeleteLoading}] = postAPI.useDeletePostMutation()
 	const { data: posts, error, isLoading, refetch } = postAPI.useFetchAllPostsQuery(limit, {
 		// pollingInterval: 1000
 	})
@@ -15,14 +17,22 @@ export default function PostContainer() {
 		const body = prompt('Type body of post', 'It`s a default body');
 		await createPost({title, body} as IPost)
 	}
-	
+	const handleUpdate = (post: IPost) => {
+		updatePost(post)
+	}
+	const handleRemove = (post: IPost) => {
+		deletePost(post)
+	}
 	return (
 		<div className=' '>
-				<button onClick={() => handleCreate()}>Add new post</button>
+				<div className='flex justify-between'>
+					<button onClick={refetch}>Refetch posts</button>
+					<button onClick={() => handleCreate()}>Add new post</button>
+				</div>
 				{!posts && isLoading && <h1 className='grid place-items-center h-screen text-center'>Loading...</h1>}
 				{error && <h1 className='grid place-items-center h-screen text-center  text-red-400' >Error 404</h1>}
 				{posts && posts.map(post => 
-					<PostItem key={post.id} post={post}/>
+					<PostItem remove={handleRemove} update={handleUpdate} key={post.id} post={post}/>
 				)}
 			</div>
 	)
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -3,16 +3,26 @@ import { IPost } from '../models/IPost'
 
 interface PostItemProps {
 	post: IPost;
+	remove: (post: IPost) => void;
+	update: (post: IPost) => void;
 }
 
-export const PostItem: FC<PostItemProps> = ({post}) => {
-	return (
-		<div className=' flex justify-between gap-5 m-5 p-2 border'>
+export const PostItem: FC<PostItemProps> = ({post, remove, update}) => {
+	const handleRemove = (event: React.MouseEvent) => {
+		event.stopPropagation();
+		remove(post)
+	}
+	const handleUpdate = (event: React.MouseEvent) => {
+		const title = prompt('Type the updated title', 'It`s an updated default title') || 'Default title';
+		update({...post, title})
+	}
+	return ( 
+		<div onClick={handleUpdate} className=' flex justify-between gap-5 m-5 p-2 border'>
 			<div>
 				<h1 className='my-2 text-black'>{post.id}. {post.title}</h1>
 				<p>{post.body}</p>
 			</div>
-			<button  className='border px-3 py-2 bg-slate-300 self-center '>Delete</button>
+			<button onClick={handleRemove} className='border px-3 py-2 bg-slate-300 self-center '>Delete</button>
 		</div>
 	)
 }
